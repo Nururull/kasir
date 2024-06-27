41a790778d017f9b336dc5484f6e6477deb02324
nururull <srnurulx@gmail.com>
2024-06-27 15:00:24 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
